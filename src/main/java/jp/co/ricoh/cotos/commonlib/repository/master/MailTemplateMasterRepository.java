107dbfd7f3efeac00445f1b4a8dd793936de2862
yamauchi <yasutaka.yamauchi@jrits.ricoh.co.jp>
2018-06-27 15:38:31 +0900

    Revert "#1373 メール送信リファレンス実装"

--- src/main/java/jp/co/ricoh/cotos/commonlib/repository/master/MailTemplateMasterRepository.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/master/MailTemplateMasterRepository.java
@@ -3,12 +3,12 @@ package jp.co.ricoh.cotos.commonlib.repository.master;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import jp.co.ricoh.cotos.commonlib.entity.communication.Communication.CommunicationCategory;
+import jp.co.ricoh.cotos.commonlib.entity.communication.Communication.ProcessCategory;
 import jp.co.ricoh.cotos.commonlib.entity.master.MailTemplateMaster;
-import jp.co.ricoh.cotos.commonlib.entity.master.MailTemplateMaster.CommunicationCategory;
-import jp.co.ricoh.cotos.commonlib.entity.master.MailTemplateMaster.ServiceCategory;
 
 @Repository
 public interface MailTemplateMasterRepository extends CrudRepository<MailTemplateMaster, Long> {
-	public MailTemplateMaster findByServiceCategoryAndCommunicationCategory(ServiceCategory serviceCategory, CommunicationCategory communicationCategory);
+	public MailTemplateMaster findByCommunicationCategoryAndProcessCategory(CommunicationCategory communicationCategory, ProcessCategory processCaetgory);
 
 }
