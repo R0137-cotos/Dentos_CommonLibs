893769a99fb625f007d2e9c63bc578955819d431
yamauchi <yasutaka.yamauchi@jrits.ricoh.co.jp>
2018-06-28 13:18:15 +0900

    Revert "Revert "#1152 like検索時のパラメータのうち、"_"と"%"をエスケープした""

--- src/test/java/jp/co/ricoh/cotos/commonlib/TestDBUtil.java
+++ src/test/java/jp/co/ricoh/cotos/commonlib/TestDBUtil.java
@@ -1,6 +1,8 @@
 package jp.co.ricoh.cotos.commonlib;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.IntStream;
 
 import javax.transaction.Transactional;
@@ -29,10 +31,68 @@ public class TestDBUtil {
 			long beforeCount = dbUtil.loadFromSQLFile("sql/testLoadAll.sql", TestData.class).size();
 			long beforeCountWithCountSql = dbUtil.loadCountFromSQLFile("sql/testCount.sql", Collections.emptyMap());
 			Assert.assertEquals("listで取ったものと、Countで取ったものと件数が一致する。", beforeCount, beforeCountWithCountSql);
-			val moji = "どうでしょう?" + System.currentTimeMillis();
-			dbUtil.execute("sql/testInsert.sql", Collections.singletonMap("moji", moji));
+
+			Map<String, Object> testParam = new HashMap<>();
+
+			long time = System.currentTimeMillis();
+			val moji = "どうでしょう?" + time;
+			val likeSearchString = "test" + time;
+			testParam.put("moji", moji);
+			testParam.put("likeSearchString", likeSearchString);
+
+			dbUtil.execute("sql/testInsert.sql", testParam);
 			Assert.assertEquals("インサート後にデータが増えている。", beforeCount + 1, dbUtil.loadFromSQLFile("sql/testLoadAll.sql", TestData.class).size());
 			Assert.assertEquals("文字が入ってる。", moji, dbUtil.loadSingleFromSQLFile("sql/testLoadMaxId.sql", TestData.class).getMoji());
 		});
+	}
+
+	@Test
+	@Transactional
+	public void Like句のエスケープが正常に実施されることを確認() {
+
+		Map<String, Object> testDataUnderscore = new HashMap<>();
+		testDataUnderscore.put("moji", "半角アンダースコア");
+		testDataUnderscore.put("likeSearchString", "ai_eo");
+		dbUtil.execute("sql/testInsert.sql", testDataUnderscore);
+
+		Map<String, Object> testDataUnderscoreMulti = new HashMap<>();
+		testDataUnderscoreMulti.put("moji", "全角アンダースコア");
+		testDataUnderscoreMulti.put("likeSearchString", "ai＿eo");
+		dbUtil.execute("sql/testInsert.sql", testDataUnderscoreMulti);
+
+		Map<String, Object> testDataPercent = new HashMap<>();
+		testDataPercent.put("moji", "半角パーセント");
+		testDataPercent.put("likeSearchString", "ai%eo");
+		dbUtil.execute("sql/testInsert.sql", testDataPercent);
+
+		Map<String, Object> testDataPercentMulti = new HashMap<>();
+		testDataPercentMulti.put("moji", "全角パーセント");
+		testDataPercentMulti.put("likeSearchString", "ai％eo");
+		dbUtil.execute("sql/testInsert.sql", testDataPercentMulti);
+
+		Map<String, Object> testDataEscape = new HashMap<>();
+		testDataEscape.put("moji", "エスケープ文字");
+		testDataEscape.put("likeSearchString", "ai\\％eo");
+		dbUtil.execute("sql/testInsert.sql", testDataEscape);
+
+		// 半角アンダースコア
+		Assert.assertEquals("Like検索件数が1件であること", 1, dbUtil.loadFromSQLFile("sql/testLoadLikeSearch.sql", TestData.class, Collections.singletonMap("likeSearchString", "i_e")).size());
+		Assert.assertEquals("Like検索できること", "半角アンダースコア", dbUtil.loadFromSQLFile("sql/testLoadLikeSearch.sql", TestData.class, Collections.singletonMap("likeSearchString", "i_e")).get(0).getMoji());
+
+		// 全角アンダースコア
+		Assert.assertEquals("Like検索件数が1件であること", 1, dbUtil.loadFromSQLFile("sql/testLoadLikeSearch.sql", TestData.class, Collections.singletonMap("likeSearchString", "i＿e")).size());
+		Assert.assertEquals("Like検索できること", "全角アンダースコア", dbUtil.loadFromSQLFile("sql/testLoadLikeSearch.sql", TestData.class, Collections.singletonMap("likeSearchString", "i＿e")).get(0).getMoji());
+
+		// 半角パーセント
+		Assert.assertEquals("Like検索件数が1件であること", 1, dbUtil.loadFromSQLFile("sql/testLoadLikeSearch.sql", TestData.class, Collections.singletonMap("likeSearchString", "i%e")).size());
+		Assert.assertEquals("Like検索できること", "半角パーセント", dbUtil.loadFromSQLFile("sql/testLoadLikeSearch.sql", TestData.class, Collections.singletonMap("likeSearchString", "i%e")).get(0).getMoji());
+
+		// 全角パーセント
+		Assert.assertEquals("Like検索件数が1件であること", 1, dbUtil.loadFromSQLFile("sql/testLoadLikeSearch.sql", TestData.class, Collections.singletonMap("likeSearchString", "i％e")).size());
+		Assert.assertEquals("Like検索できること", "全角パーセント", dbUtil.loadFromSQLFile("sql/testLoadLikeSearch.sql", TestData.class, Collections.singletonMap("likeSearchString", "i％e")).get(0).getMoji());
+
+		// エスケープ文字で検索できることの確認
+		Assert.assertEquals("Like検索件数が1件であること", 1, dbUtil.loadFromSQLFile("sql/testLoadLikeSearch.sql", TestData.class, Collections.singletonMap("likeSearchString", "\\％")).size());
+		Assert.assertEquals("Like検索できること", "エスケープ文字", dbUtil.loadFromSQLFile("sql/testLoadLikeSearch.sql", TestData.class, Collections.singletonMap("likeSearchString", "\\％")).get(0).getMoji());
 	}
 }
