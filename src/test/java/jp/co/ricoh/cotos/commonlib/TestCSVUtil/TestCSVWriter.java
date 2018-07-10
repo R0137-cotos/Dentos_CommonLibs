0bdb9bc1244447281c6977ffade014f1ddedf798
mkanda <masashi.kanda@jp.ricoh.com>
2018-07-10 10:47:05 +0900

    CIテストエラー検証のための一時コミットその２

--- src/test/java/jp/co/ricoh/cotos/commonlib/TestCSVUtil/TestCSVWriter.java
+++ src/test/java/jp/co/ricoh/cotos/commonlib/TestCSVUtil/TestCSVWriter.java
@@ -301,9 +301,7 @@ public class TestCSVWriter {
 		} catch (ErrorCheckException e) {
 			file.setWritable(true);
 
-			e.getErrorInfoList().stream().forEach(s -> {
-				System.out.println(s.getErrorId() + ":" + s.getErrorMessage());
-			});
+			Assert.assertTrue("エラーリストの数が期待結果通りであること", e.getErrorInfoList().size() == 3);
 
 			Assert.assertEquals("エラーIDが正しく設定されること", "ROT00106", e.getErrorInfoList().get(0).getErrorId());
 			Assert.assertEquals("エラーメッセージが正しく設定されること", file.getAbsolutePath() + "の復元に失敗しました。", e.getErrorInfoList().get(0).getErrorMessage());
