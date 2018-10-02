7ab47100ec3934aff4a8c609f96f46e80129b312
yamauchi <yasutaka.yamauchi@jrits.ricoh.co.jp>
2018-10-02 10:49:49 +0900

    ステータス修正

--- src/main/java/jp/co/ricoh/cotos/commonlib/dto/parameter/EstimationDetailRegisterParameter.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/parameter/EstimationDetailRegisterParameter.java
@@ -1,37 +1,18 @@
 package jp.co.ricoh.cotos.commonlib.dto.parameter;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
 
 import javax.persistence.Lob;
-
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
 
 import lombok.Data;
 
 @Data
 public class EstimationDetailRegisterParameter {
 
-	public enum Status {
-
-		ADD, DELETE, ー;
-
-		@JsonValue
-		public String toValue() {
-			return this.name();
-		}
-
-		@JsonCreator
-		public static Enum<Status> fromValue(String name) {
-			return Arrays.stream(values()).filter(v -> v.name() == name).findFirst().orElseThrow(() -> new IllegalArgumentException(String.valueOf(name)));
-		}
-	}
-
 	/**
 	 * ステータス
 	 */
-	private Status status;
+	private String status;
 
 	/**
 	 * 品種コード
