ecae2ca94fbf5d621df28e5da967add48d9b9cff
chan-asa <Takahiro.Asakura@jrits.ricoh.co.jp>
2018-10-03 11:27:17 +0900

    ユーザー権限取得のリポジトリーを追加

---
+++ src/main/java/jp/co/ricoh/cotos/commonlib/entity/master/DispUrlAuthMaster.java
@@ -0,0 +1,71 @@
+package jp.co.ricoh.cotos.commonlib.entity.master;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import io.swagger.annotations.ApiModelProperty;
+import jp.co.ricoh.cotos.commonlib.entity.EntityBaseMaster;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 画面URL権限マスタ
+ */
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Table(name = "disp_url_auth_master")
+public class DispUrlAuthMaster extends EntityBaseMaster {
+
+	@Embeddable
+	@Data
+	public static class Id implements Serializable {
+
+		/**
+		 * シリアルバージョンID
+		 */
+		private static final long serialVersionUID = 1L;
+
+		/**
+		 * システムドメイン
+		 */
+		@Column(nullable = false)
+		@ApiModelProperty(value = "システムドメイン", required = true, position = 1)
+		private String systemDomain;
+
+		/**
+		 * URLパターン
+		 */
+		@Column(nullable = false)
+		@ApiModelProperty(value = "URLパターン", required = true, position = 2)
+		private String urlPattern;
+
+		/**
+		 * アクションID
+		 */
+		@Column(nullable = false)
+		@ApiModelProperty(value = "アクションID", required = true, position = 3)
+		private String actionId;
+	}
+
+	@EmbeddedId
+	private Id id;
+
+	/**
+	 * アクション名
+	 */
+	@ApiModelProperty(value = "アクション名", required = false, position = 4)
+	private String actionName;
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "authPatternId")
+	@ApiModelProperty(value = "権限パターンマスタ", required = true, position = 5)
+	private AuthPatternMaster authPatternMaster;
+}
---
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/master/JwtSysAuthMasterRepository.java
@@ -0,0 +1,11 @@
+package jp.co.ricoh.cotos.commonlib.repository.master;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import jp.co.ricoh.cotos.commonlib.entity.master.JwtSysAuthMaster;
+
+@Repository
+public interface JwtSysAuthMasterRepository extends CrudRepository<JwtSysAuthMaster, String> {
+	public JwtSysAuthMaster findBySystemIdAndPassword(String systemId, String password);
+}
