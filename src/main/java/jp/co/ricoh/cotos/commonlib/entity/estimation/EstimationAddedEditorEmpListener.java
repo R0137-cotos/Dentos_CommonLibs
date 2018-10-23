2bbc246ed693b29c108f7a30f6f58109574833b6
yamauchi <yasutaka.yamauchi@jrits.ricoh.co.jp>
2018-10-23 12:44:18 +0900

    リスナークラスを追加

---
+++ src/main/java/jp/co/ricoh/cotos/commonlib/entity/estimation/EstimationAddedEditorEmpListener.java
@@ -0,0 +1,68 @@
+package jp.co.ricoh.cotos.commonlib.entity.estimation;
+
+import java.util.ArrayList;
+
+import javax.persistence.PrePersist;
+import javax.transaction.Transactional;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import jp.co.ricoh.cotos.commonlib.entity.master.MvEmployeeMaster;
+import jp.co.ricoh.cotos.commonlib.exception.ErrorCheckException;
+import jp.co.ricoh.cotos.commonlib.exception.ErrorInfo;
+import jp.co.ricoh.cotos.commonlib.logic.check.CheckUtil;
+import jp.co.ricoh.cotos.commonlib.repository.master.MvEmployeeMasterRepository;
+
+@Component
+public class EstimationAddedEditorEmpListener {
+
+	private static MvEmployeeMasterRepository mvEmployeeMasterRepository;
+
+	@Autowired
+	public void setMvEmployeeMasterRepository(MvEmployeeMasterRepository mvEmployeeMasterRepository) {
+		EstimationAddedEditorEmpListener.mvEmployeeMasterRepository = mvEmployeeMasterRepository;
+	}
+
+	@Autowired
+	CheckUtil checkUtil;
+
+	/**
+	 * 社員マスタ情報を見積追加編集者トランザクションに紐づけます。
+	 *
+	 * @param entity
+	 */
+	@PrePersist
+	@Transactional
+	public void appendsEmployeeFields(EstimationAddedEditorEmp estimationAddedEditorEmp) {
+		MvEmployeeMaster employeeMaster = mvEmployeeMasterRepository.findByMomEmployeeId(estimationAddedEditorEmp.getMvEmployeeMaster().getMomEmployeeId());
+
+		if (employeeMaster == null) {
+			String[] regexList = { "見積追加編集者社員" };
+			throw new ErrorCheckException(checkUtil.addErrorInfo(new ArrayList<ErrorInfo>(), "MasterDoesNotExistEmployeeMaster", regexList));
+		}
+
+		BeanUtils.copyProperties(employeeMaster, estimationAddedEditorEmp);
+		estimationAddedEditorEmp.setEmployeeName(employeeMaster.getJobname1() + employeeMaster.getJobname2());
+		estimationAddedEditorEmp.setAddress(convertJoinedAddress(employeeMaster));
+		estimationAddedEditorEmp.setMvEmployeeMaster(employeeMaster);
+	}
+
+	private String convertJoinedAddress(MvEmployeeMaster master) {
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(StringUtils.defaultIfEmpty(master.getTdhknNmKnji(), StringUtils.EMPTY));
+		sb.append(StringUtils.defaultIfEmpty(master.getSkugnchosnKnji(), StringUtils.EMPTY));
+		sb.append(StringUtils.defaultIfEmpty(master.getOwaTusyoKnji(), StringUtils.EMPTY));
+		sb.append(StringUtils.defaultIfEmpty(master.getKowChomeKnji(), StringUtils.EMPTY));
+		sb.append(StringUtils.defaultIfEmpty(master.getStreet(), StringUtils.EMPTY));
+		sb.append(StringUtils.defaultIfEmpty(master.getBuilding(), StringUtils.EMPTY));
+
+		return sb.toString();
+	}
+
+	
+}
