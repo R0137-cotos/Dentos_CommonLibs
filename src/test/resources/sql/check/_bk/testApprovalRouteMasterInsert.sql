dabf66c97c0b8e7bf5bfb948c6efa4c30d8e9fc2
kakuhana <kentaroh.kakuhana@jrits.ricoh.co.jp>
2018-10-25 13:31:16 +0900

    SQLファイル削除

--- src/test/resources/sql/check/_bk/testApprovalRouteMasterInsert.sql
+++
@@ -1,8 +0,0 @@
-insert into approval_route_master(id,approval_route_name,cond_determine_order,description,route_condition_formula,special_price_approval_flg,approval_route_grp_id)
-values(1,'特価対象フラグon',1,null,'function () { return true; }',1,1);
-insert into approval_route_master(id,approval_route_name,cond_determine_order,description,route_condition_formula,special_price_approval_flg,approval_route_grp_id)
-values(2,'特価対象フラグon',1,null,'function () { return true; }',1,2);
-insert into approval_route_master(id,approval_route_name,cond_determine_order,description,route_condition_formula,special_price_approval_flg,approval_route_grp_id)
-values(3,'特価対象フラグoff',1,null,'function () { validate("解約日",estimation.coverPresentationDate); return estimation.coverPresentationDate == 4980; }',1,3);
-insert into approval_route_master(id,approval_route_name,cond_determine_order,description,route_condition_formula,special_price_approval_flg,approval_route_grp_id)
-values(4,'特価対象フラグoff',1,null,'function () { validate("掲示日",contract.cancelScheduledDate); return contract.cancelScheduledDate == 4980; }',1,4);
--- src/test/resources/sql/check/_bk/testArrangementMasterInsert.sql
+++
@@ -1,2 +0,0 @@
-insert into arrangement_master(id,name,arrangement_term_start,arrangement_term_end,description)
-values(1,'テスト手配業務1',systimestamp,systimestamp,null);
