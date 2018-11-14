1707349c43ffdc82eea8352efcde74b735680088
mkanda <masashi.kanda@jp.ricoh.com>
2018-11-14 12:55:46 +0900

    WIP

--- src/main/java/jp/co/ricoh/cotos/commonlib/entity/master/RecordDecomposeCompMaster.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/entity/master/RecordDecomposeCompMaster.java
@@ -10,6 +10,8 @@ import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import io.swagger.annotations.ApiModelProperty;
 import jp.co.ricoh.cotos.commonlib.entity.EntityBaseMaster;
 import jp.co.ricoh.cotos.commonlib.entity.EnumType.TargetContractType;
@@ -36,6 +38,7 @@ public class RecordDecomposeCompMaster extends EntityBaseMaster {
 	 */
 	@ManyToOne(optional = false)
 	@JoinColumn(name = "item_master_id", referencedColumnName = "id")
+	@JsonIgnore
 	@ApiModelProperty(value = "品種マスタ", required = true, position = 2)
 	private ItemMaster itemMaster;
 
