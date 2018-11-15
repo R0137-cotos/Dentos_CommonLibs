a30f9772c81d840447586e3d4ed4b6038b6c164e
takeshi sakuma <takeshi.sakuma2@jp.ricoh.com>
2018-11-15 15:00:12 +0900

    TODO対応

--- src/main/java/jp/co/ricoh/cotos/commonlib/security/BufferedServletInputStream.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/security/BufferedServletInputStream.java
@@ -31,19 +31,16 @@ public class BufferedServletInputStream extends ServletInputStream {
 
 	@Override
 	public boolean isFinished() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean isReady() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public void setReadListener(ReadListener listener) {
-		// TODO Auto-generated method stub
-		
+
 	}
 }
