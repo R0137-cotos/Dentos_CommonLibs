1d12a8bd3f7d127dae2d726919764dadc073c006
kamada <tatsuya.kamada@jrits.ricoh.co.jp>
2018-11-20 17:14:05 +0900

    一覧取得DTO再修正

--- src/main/java/jp/co/ricoh/cotos/commonlib/dto/result/ContractListInfo.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/result/ContractListInfo.java
@@ -2,6 +2,8 @@ package jp.co.ricoh.cotos.commonlib.dto.result;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -17,10 +19,15 @@ import lombok.Data;
  * 一覧を取得するためには、こちらのクラスを使ってください。 <br/>
  * このクラスを使用してDBへの保存を行うことは出来ません。
  */
+@Entity
 @Data
 public class ContractListInfo {
 
-	@ApiModelProperty(value = "契約ID", required = true, position = 1)
+	@Id
+	@ApiModelProperty(value = "連番", required = true, position = 1)
+	private long seqNo;
+
+	@ApiModelProperty(value = "契約ID", required = true, position = 2)
 	private long id;
 
 	/**
@@ -28,13 +35,13 @@ public class ContractListInfo {
 	 */
 	@ApiModelProperty(value = "契約番号<br />" //
 			+ "契約番号 + \"-\" + 契約番号枝番", //
-			required = false, position = 2, allowableValues = "range[0,18]") //
+			required = false, position = 3, allowableValues = "range[0,18]") //
 	private String contractNumber;
 
 	/**
 	 * サービス識別番号
 	 */
-	@ApiModelProperty(value = "サービス識別番号", required = false, position = 3, allowableValues = "range[0,18]")
+	@ApiModelProperty(value = "サービス識別番号", required = false, position = 4, allowableValues = "range[0,18]")
 	private String serviceIdentificationNumber;
 
 	/**
@@ -42,7 +49,7 @@ public class ContractListInfo {
 	 */
 	@ApiModelProperty(value = "契約種別<br />" //
 			+ "新規, プラン変更, 解約などの契約種別を表す。", //
-			required = false, position = 4) //
+			required = false, position = 5) //
 	private ContractType contractType;
 
 	/**
@@ -50,7 +57,7 @@ public class ContractListInfo {
 	 */
 	@ApiModelProperty(value = "契約ステータス<br />" //
 			+ "状態遷移上のライフサイクル状態を表す。", //
-			required = false, position = 5) //
+			required = false, position = 6) //
 	private LifecycleStatus contractStatus;
 
 	/**
@@ -58,89 +65,89 @@ public class ContractListInfo {
 	 */
 	@ApiModelProperty(value = "契約状態<br />" //
 			+ "状態遷移上のワークフロー状態を表す。", //
-			required = false, position = 6) //
+			required = false, position = 7) //
 	private WorkflowStatus contractCondition;
 
 	/**
 	 * 見積ID
 	 */
-	@ApiModelProperty(value = "見積ID", required = true, position = 7)
+	@ApiModelProperty(value = "見積ID", required = true, position = 8)
 	private long estimationId;
-	
+
 	/**
 	 * 見積番号
 	 */
 	@ApiModelProperty(value = "見積番号<br />" //
 			+ "見積番号 + \"-\" + 見積番号枝番", //
-			required = false, position = 8, allowableValues = "range[0,18]") //
+			required = false, position = 9, allowableValues = "range[0,18]") //
 	private String estimateNumber;
 
 	/**
 	 * 見積件名
 	 */
-	@ApiModelProperty(value = "見積件名", required = false, position = 9, allowableValues = "range[0,255]")
+	@ApiModelProperty(value = "見積件名", required = false, position = 10, allowableValues = "range[0,255]")
 	private String estimationTitle;
 
 	/**
 	 * 案件番号
 	 */
-	@ApiModelProperty(value = "案件番号", required = false, position = 10, allowableValues = "range[0,255]")
+	@ApiModelProperty(value = "案件番号", required = false, position = 11, allowableValues = "range[0,255]")
 	private String caseNumber;
 
 	/**
 	 * 請求開始月
 	 */
-	@ApiModelProperty(value = "請求開始月", required = false, position = 11)
+	@ApiModelProperty(value = "請求開始月", required = false, position = 12)
 	private String billingStartMonth;
 
 	/**
 	 * お客様企業名
 	 */
-	@ApiModelProperty(value = "お客様企業名", required = false, position = 12, allowableValues = "range[0,255]")
+	@ApiModelProperty(value = "お客様企業名", required = false, position = 13, allowableValues = "range[0,255]")
 	private String companyName;
 
 	/**
 	 * お客様事業所名
 	 */
-	@ApiModelProperty(value = "お客様事業所名", required = false, position = 13, allowableValues = "range[0,255]")
+	@ApiModelProperty(value = "お客様事業所名", required = false, position = 14, allowableValues = "range[0,255]")
 	private String officeName;
 
 	/**
 	 * お客様部門名
 	 */
-	@ApiModelProperty(value = "お客様部門名", required = false, position = 14, allowableValues = "range[0,255]")
+	@ApiModelProperty(value = "お客様部門名", required = false, position = 15, allowableValues = "range[0,255]")
 	private String departmentName;
 
 	/**
 	 * サービス開始日
 	 */
 	@Temporal(TemporalType.TIMESTAMP)
-	@ApiModelProperty(value = "サービス開始日", required = false, position = 15)
+	@ApiModelProperty(value = "サービス開始日", required = false, position = 16)
 	private Date serviceTermStart;
 
 	/**
 	 * サービス終了日
 	 */
 	@Temporal(TemporalType.TIMESTAMP)
-	@ApiModelProperty(value = "サービス終了日", required = false, position = 16)
+	@ApiModelProperty(value = "サービス終了日", required = false, position = 17)
 	private Date serviceTermEnd;
 
 	/**
 	 * 商品名称
 	 */
-	@ApiModelProperty(value = "商品名称", required = false, position = 17, allowableValues = "range[0,255]")
+	@ApiModelProperty(value = "商品名称", required = false, position = 18, allowableValues = "range[0,255]")
 	private String productName;
 
 	/**
 	 * 担当営業氏名
 	 */
-	@ApiModelProperty(value = "担当営業氏名", required = false, position = 18, allowableValues = "range[0,255]")
+	@ApiModelProperty(value = "担当営業氏名", required = false, position = 19, allowableValues = "range[0,255]")
 	private String picEmptxName;
 
 	/**
 	 * 担当支社名
 	 */
-	@ApiModelProperty(value = "担当支社名", required = false, position = 19, allowableValues = "range[0,255]")
+	@ApiModelProperty(value = "担当支社名", required = false, position = 20, allowableValues = "range[0,255]")
 	private String picAffiliateName;
 
 	@PrePersist
--- src/main/java/jp/co/ricoh/cotos/commonlib/dto/result/EstimationListInfo.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/result/EstimationListInfo.java
@@ -2,6 +2,8 @@ package jp.co.ricoh.cotos.commonlib.dto.result;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -16,16 +18,21 @@ import lombok.Data;
  * 一覧を取得するためには、こちらのクラスを使ってください。 <br/>
  * このクラスを使用してDBへの保存を行うことは出来ません。
  */
+@Entity
 @Data
 public class EstimationListInfo {
 
-	@ApiModelProperty(value = "見積ID", required = true, position = 1)
+	@Id
+	@ApiModelProperty(value = "連番", required = true, position = 1)
+	private long seqNo;
+	
+	@ApiModelProperty(value = "見積ID", required = true, position = 2)
 	private long id;
 
 	/**
 	 * 見積番号
 	 */
-	@ApiModelProperty(value = "見積番号", required = true, position = 2, allowableValues = "range[0,18]")
+	@ApiModelProperty(value = "見積番号", required = true, position = 3, allowableValues = "range[0,18]")
 	private String estimateNumber;
 
 	/**
@@ -33,13 +40,13 @@ public class EstimationListInfo {
 	 */
 	@ApiModelProperty(value = "見積種別<br />" //
 			+ "新規、プラン変更等の見積種別を表す。", //
-			required = false, position = 3) //
+			required = false, position = 4) //
 	private EstimationType estimationtype;
 
 	/**
 	 * 見積件名
 	 */
-	@ApiModelProperty(value = "見積件名", required = false, position = 4, allowableValues = "range[0,255]")
+	@ApiModelProperty(value = "見積件名", required = false, position = 5, allowableValues = "range[0,255]")
 	private String estimationTitle;
 
 	/**
@@ -47,56 +54,56 @@ public class EstimationListInfo {
 	 */
 	@ApiModelProperty(value = "見積ステータス<br />" //
 			+ "状態遷移上のワークフロー状態を表す。", //
-			required = false, position = 5) //
+			required = false, position = 6) //
 	private WorkflowStatus status;
 
 	/**
 	 * 案件番号
 	 */
-	@ApiModelProperty(value = "案件番号", required = false, position = 6, allowableValues = "range[0,255]")
+	@ApiModelProperty(value = "案件番号", required = false, position = 7, allowableValues = "range[0,255]")
 	private String caseNumber;
 
 	/**
 	 * 事業所名
 	 */
-	@ApiModelProperty(value = "事業所名", required = false, position = 7, allowableValues = "range[0,255]")
+	@ApiModelProperty(value = "事業所名", required = false, position = 8, allowableValues = "range[0,255]")
 	private String officeName;
 
 	/**
 	 * 企業名
 	 */
-	@ApiModelProperty(value = "企業名", required = false, position = 8, allowableValues = "range[0,255]")
+	@ApiModelProperty(value = "企業名", required = false, position = 9, allowableValues = "range[0,255]")
 	private String customerName;
 
 	/**
 	 * 部門名
 	 */
-	@ApiModelProperty(value = "部門名", required = false, position = 9, allowableValues = "range[0,255]")
+	@ApiModelProperty(value = "部門名", required = false, position = 10, allowableValues = "range[0,255]")
 	private String departmentName;
 
 	/**
 	 * 担当営業
 	 */
-	@ApiModelProperty(value = "担当営業", required = false, position = 10, allowableValues = "range[0,8]")
+	@ApiModelProperty(value = "担当営業", required = false, position = 11, allowableValues = "range[0,8]")
 	private String picEmptxName;
 
 	/**
 	 * 商品名
 	 */
-	@ApiModelProperty(value = "商品名", required = false, position = 11, allowableValues = "range[0,255]")
+	@ApiModelProperty(value = "商品名", required = false, position = 12, allowableValues = "range[0,255]")
 	private String itemName;
 
 	/**
 	 * 掲示日
 	 */
-	@ApiModelProperty(value = "掲示日", required = false, position = 12)
+	@ApiModelProperty(value = "掲示日", required = false, position = 13)
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date coverPresentationDate;
 
 	/**
 	 * 担当支社名
 	 */
-	@ApiModelProperty(value = "担当支社名", required = false, position = 13, allowableValues = "range[0,255]")
+	@ApiModelProperty(value = "担当支社名", required = false, position = 14, allowableValues = "range[0,255]")
 	private String picAffiliateName;
 
 	@PrePersist
