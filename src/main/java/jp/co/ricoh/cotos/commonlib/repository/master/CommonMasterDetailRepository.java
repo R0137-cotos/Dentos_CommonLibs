be04707446fc244ab2c1a44c4ce9f185de29ef04
Tatsuya Honma <tatsuya.honma@jrits.ricoh.co.jp>
2018-12-27 14:52:36 +0900

    リポジトリ、Enum追加・修正

--- src/main/java/jp/co/ricoh/cotos/commonlib/repository/master/CommonMasterDetailRepository.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/master/CommonMasterDetailRepository.java
@@ -13,4 +13,8 @@ import jp.co.ricoh.cotos.commonlib.entity.master.CommonMasterDetail;
 public interface CommonMasterDetailRepository extends CrudRepository<CommonMasterDetail, Long> {
 	@Query(value = "FROM CommonMasterDetail WHERE common_master_id = :COMMON_MASTER_ID AND trunc(current_date) between trunc(available_period_from) and trunc(available_period_to) AND delete_flg = 0 order by display_order")
 	public List<CommonMasterDetail> findByCommonMasterId(@Param("COMMON_MASTER_ID") Long commonMasterId);
+
+	// 受注完了時のメール送信でのみ使用可能
+	@Query(value = "FROM CommonMasterDetail WHERE common_master_id = :COMMON_MASTER_ID AND trunc(current_date) between trunc(available_period_from) and trunc(available_period_to) AND data_area_1 = :DATA_AREA_1 AND delete_flg = 0 order by display_order")
+	public CommonMasterDetail findByCommonMasterIdAndDataArea1(@Param("COMMON_MASTER_ID") Long commonMasterId, @Param("DATA_AREA_1") String dataArea1);
 }
--- src/main/java/jp/co/ricoh/cotos/commonlib/repository/master/CommonMasterRepository.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/master/CommonMasterRepository.java
@@ -13,4 +13,8 @@ import jp.co.ricoh.cotos.commonlib.entity.master.CommonMaster;
 public interface CommonMasterRepository extends CrudRepository<CommonMaster, Long> {
 	@Query(value = "SELECT * FROM common_master WHERE (service_category = :SERVICE_CATEGORY OR (service_category = '0' AND column_name NOT IN (SELECT column_name FROM common_master WHERE service_category = :SERVICE_CATEGORY))) AND delete_flg = '0' order by id", nativeQuery = true)
 	public List<CommonMaster> findByServiceCategory(@Param("SERVICE_CATEGORY") String serviceCategory);
+
+	// 受注完了時のメール送信でのみ使用可能
+	@Query(value = "SELECT * FROM common_master WHERE (service_category = :SERVICE_CATEGORY OR (service_category = '0' AND column_name NOT IN (SELECT column_name FROM common_master WHERE service_category = :SERVICE_CATEGORY))) AND COLUMN_NAME = :COLUMN_NAME AND delete_flg = '0' order by id", nativeQuery = true)
+	public CommonMaster findByServiceCategoryAndColumnName(@Param("SERVICE_CATEGORY") String serviceCategory, @Param("COLUMN_NAME") String columnName);
 }
