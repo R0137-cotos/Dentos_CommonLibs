c193174d832f0228ca25bab27ee117821a003a9a
chan-asa <Takahiro.Asakura@jrits.ricoh.co.jp>
2018-12-27 16:26:42 +0900

    問い合わせ一覧検索用メソッドを作成

--- src/main/java/jp/co/ricoh/cotos/commonlib/repository/communication/ContactRepository.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/communication/ContactRepository.java
@@ -1,11 +1,14 @@
 package jp.co.ricoh.cotos.commonlib.repository.communication;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import jp.co.ricoh.cotos.commonlib.entity.EnumType.ServiceCategory;
 import jp.co.ricoh.cotos.commonlib.entity.communication.Contact;
 
 @Repository
 public interface ContactRepository extends CrudRepository<Contact, Long> {
-
+	public List<Contact> findByEstimationIdAndServiceCategory(long estimationId, ServiceCategory serviceCategory);
 }
