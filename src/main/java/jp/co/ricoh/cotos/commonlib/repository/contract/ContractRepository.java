c301353df4f6d95c7beabf486caaacdfd5a27a40
kamada <tatsuya.kamada@jrits.ricoh.co.jp>
2019-01-16 12:49:42 +0900

    契約リポジトリメソッド追加

--- src/main/java/jp/co/ricoh/cotos/commonlib/repository/contract/ContractRepository.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/contract/ContractRepository.java
@@ -1,5 +1,6 @@
 package jp.co.ricoh.cotos.commonlib.repository.contract;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
@@ -22,4 +23,7 @@ public interface ContractRepository extends CrudRepository<Contract, Long> {
 	public List<Contract> findByRjManageNumber(String rjManageNumber);
 
 	public List<Contract> findByEstimationIdOrderByContractBranchNumberAsc(long estimationId);
+
+	@Query(value = "SELECT * FROM CONTRACT WHERE CONTRACT_TYPE = '3' AND CHANGE_PREFERRED_DATE <= :changePreferredDate", nativeQuery = true)
+	public List<Contract> findByContractTypeAndChangePreferredDate(@Param("changePreferredDate") Date changePreferredDate);
 }
