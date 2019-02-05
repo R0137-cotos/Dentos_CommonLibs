b7523aff2ace5189e8821f842cb3f7ed5945f72d
kamada <tatsuya.kamada@jrits.ricoh.co.jp>
2019-02-05 10:27:42 +0900

    追加修正

--- src/main/java/jp/co/ricoh/cotos/commonlib/dto/parameter/estimation/EstimationAttachedFileDto.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/parameter/estimation/EstimationAttachedFileDto.java
@@ -1,7 +1,11 @@
 package jp.co.ricoh.cotos.commonlib.dto.parameter.estimation;
 
+import java.util.Date;
+
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -70,4 +74,12 @@ public class EstimationAttachedFileDto extends DtoBase {
 	@Size(max = 255)
 	@ApiModelProperty(value = "添付者組織名", required = false, position = 9, allowableValues = "range[0,255]")
 	private String attachedOrgName;
+
+	/**
+	 * 添付日時
+	 */
+	@NotNull
+	@ApiModelProperty(value = "添付日時", required = true, position = 10)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date attachedAt;
 }
