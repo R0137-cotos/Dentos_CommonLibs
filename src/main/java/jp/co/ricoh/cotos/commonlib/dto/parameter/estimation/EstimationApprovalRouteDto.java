3380764297cc89fa9b5b38f9ad4187e2da05478c
kamada <tatsuya.kamada@jrits.ricoh.co.jp>
2019-02-05 10:06:29 +0900

    POST、PUT時不要項目精査(見積)修正漏れ

--- src/main/java/jp/co/ricoh/cotos/commonlib/dto/parameter/estimation/EstimationApprovalRouteDto.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/parameter/estimation/EstimationApprovalRouteDto.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.OneToMany;
 import javax.persistence.OrderBy;
 import javax.validation.constraints.Max;
+import javax.validation.constraints.Size;
 
 import io.swagger.annotations.ApiModelProperty;
 import jp.co.ricoh.cotos.commonlib.dto.parameter.common.DtoBase;
@@ -16,10 +17,31 @@ import lombok.EqualsAndHashCode;
 public class EstimationApprovalRouteDto extends DtoBase {
 
 	/**
+	 * 承認依頼者MoM社員ID
+	 */
+	@Size(max = 255)
+	@ApiModelProperty(value = "承認依頼者MoM社員ID", required = true, position = 3, allowableValues = "range[0,255]")
+	private String approvalRequesterEmpId;
+
+	/**
+	 * 承認依頼者氏名
+	 */
+	@Size(max = 255)
+	@ApiModelProperty(value = "承認依頼者氏名", required = true, position = 4, allowableValues = "range[0,255]")
+	private String approvalRequesterName;
+
+	/**
+	 * 承認依頼者組織名
+	 */
+	@Size(max = 255)
+	@ApiModelProperty(value = "承認依頼者組織名", required = false, position = 5, allowableValues = "range[0,255]")
+	private String approvalRequesterOrgName;
+
+	/**
 	 * 特価承認対象フラグ
 	 */
 	@Max(9)
-	@ApiModelProperty(value = "特価承認対象フラグ", required = true, position = 3, allowableValues = "range[0,9]")
+	@ApiModelProperty(value = "特価承認対象フラグ", required = true, position = 6, allowableValues = "range[0,9]")
 	private int specialPriceApprovalFlg;
 
 	/**
@@ -27,6 +49,6 @@ public class EstimationApprovalRouteDto extends DtoBase {
 	 */
 	@OneToMany(mappedBy = "estimationApprovalRoute")
 	@OrderBy("approvalOrder ASC")
-	@ApiModelProperty(value = "見積承認ルートノード", required = true, position = 4)
+	@ApiModelProperty(value = "見積承認ルートノード", required = true, position = 7)
 	private List<EstimationApprovalRouteNodeDto> estimationApprovalRouteNodeList;
 }
