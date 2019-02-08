567a82ed01e90d097b732580ac693c4e9c856a11
kamada <tatsuya.kamada@jrits.ricoh.co.jp>
2019-02-08 10:29:25 +0900

    問い合わせ用DTO作成

---
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/parameter/communication/ContactToDto.java
@@ -0,0 +1,44 @@
+package jp.co.ricoh.cotos.commonlib.dto.parameter.communication;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import io.swagger.annotations.ApiModelProperty;
+import jp.co.ricoh.cotos.commonlib.dto.parameter.common.DtoBase;
+import jp.co.ricoh.cotos.commonlib.entity.communication.ContactTo.SendType;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = false)
+@Data
+public class ContactToDto extends DtoBase {
+
+	/**
+	 * 送信タイプ
+	 */
+	@ApiModelProperty(value = "送信タイプ", required = false, allowableValues = "TO(\"1\"), CC(\"2\")", example = "1", position = 3)
+	private SendType sendType;
+
+	/**
+	 * 宛先MoM社員ID
+	 */
+	@NotNull
+	@Size(max = 255)
+	@ApiModelProperty(value = "宛先MoM社員ID", required = true, position = 4, allowableValues = "range[0,255]")
+	private String contactToEmpId;
+
+	/**
+	 * 宛先メールアドレス
+	 */
+	@Size(max = 255)
+	@ApiModelProperty(value = "宛先メールアドレス ", required = false, position = 5, allowableValues = "range[0,255]")
+	private String contactToEmail;
+
+	/**
+	 * 宛先氏名
+	 */
+	@Size(max = 255)
+	@ApiModelProperty(value = "宛先氏名", required = false, position = 6, allowableValues = "range[0,255]")
+	private String contactToEmpName;
+
+}
