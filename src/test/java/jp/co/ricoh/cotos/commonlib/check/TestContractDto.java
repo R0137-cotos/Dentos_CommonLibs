813d67db12d5111811da6595c69f816726881f7e
Asakawa Tomoya <z00s600108@rfg.rfgad.ricoh.com>
2019-04-10 10:22:36 +0900

    テスト修正

--- src/test/java/jp/co/ricoh/cotos/commonlib/check/TestContractDto.java
+++ src/test/java/jp/co/ricoh/cotos/commonlib/check/TestContractDto.java
@@ -1166,11 +1166,10 @@ public class TestContractDto {
 		BeanUtils.copyProperties(dto, testTarget);
 		testTarget.setLifecycleStatus(null);
 		testTarget.setWorkflowStatus(null);
-		testTarget.setContractNumber(null);
 		testTarget.setContractPicSaEmp(null);
 		testTarget.setCustomerContract(null);
 		result = testSecurityController.callParameterCheck(testTarget, headersProperties, localServerPort);
-		Assert.assertTrue(result.getErrorInfoList().size() == 5);
+		Assert.assertTrue(result.getErrorInfoList().size() == 4);
 		Assert.assertTrue(testTool.errorIdMatchesAll(result.getErrorInfoList(), ParameterErrorIds.ROT00013));
 		Assert.assertTrue(testTool.errorMessageMatchesOne(result.getErrorInfoList(), "契約担当SA社員が設定されていません。"));
 
