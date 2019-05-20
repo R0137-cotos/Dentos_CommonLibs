47e99be25dc628699973b9e40072ef71e3aff17c
chan-asa <Takahiro.Asakura@jrits.ricoh.co.jp>
2019-05-20 13:31:22 +0900

    #3035 ルートノード系トランザクションに「承認者導出方式区分」を追加

--- src/main/java/jp/co/ricoh/cotos/commonlib/entity/estimation/EstimationApprovalRouteNode.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/entity/estimation/EstimationApprovalRouteNode.java
@@ -19,6 +19,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import io.swagger.annotations.ApiModelProperty;
 import jp.co.ricoh.cotos.commonlib.entity.EntityBase;
+import jp.co.ricoh.cotos.commonlib.entity.master.ApprovalRouteNodeMaster.ApproverDeriveMethodDiv;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -110,4 +111,9 @@ public class EstimationApprovalRouteNode extends EntityBase {
 	@ApiModelProperty(value = "代理承認者組織名", required = false, position = 10, allowableValues = "range[0,255]")
 	private String subApproverOrgName;
 
+	/**
+	 * 承認者導出方式区分
+	 */
+	@ApiModelProperty(value = "承認者導出方式区分", required = false, allowableValues = "直属上司指定(\"1\"), 組織絶対階層指定(\"2\"), 組織直接指定(\"3\"), ユーザー直接指定(\"4\")", example = "1", position = 11)
+	private ApproverDeriveMethodDiv approverDeriveMethodDiv;
 }
