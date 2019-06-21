bba44abcdd88c8907f607513c6d88111918e2982
TomosadaKohhei <kohhei.tomosada@jp.ricoh.com>
2019-06-21 16:26:38 +0900

    #3329 帳票一覧取得修正

--- src/main/java/jp/co/ricoh/cotos/commonlib/dto/parameter/reports/ReportListParameter.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/parameter/reports/ReportListParameter.java
@@ -3,11 +3,6 @@ package jp.co.ricoh.cotos.commonlib.dto.parameter.reports;
 import javax.validation.constraints.Size;
 
 import io.swagger.annotations.ApiModelProperty;
-import jp.co.ricoh.cotos.commonlib.entity.EnumType.ServiceCategory;
-import jp.co.ricoh.cotos.commonlib.entity.master.ReportTemplateMaster.LifecycleStatus;
-import jp.co.ricoh.cotos.commonlib.entity.master.ReportTemplateMaster.OutputType;
-import jp.co.ricoh.cotos.commonlib.entity.master.ReportTemplateMaster.TargetType;
-import jp.co.ricoh.cotos.commonlib.entity.master.ReportTemplateMaster.WorkflowStatus;
 import lombok.Data;
 
 /**
@@ -20,13 +15,13 @@ public class ReportListParameter {
 	 * 出力形式
 	 */
 	@ApiModelProperty(value = "出力形式", required = false, allowableValues = "PDF(\"1\"), Excel(\"2\")", example = "1", position = 1)
-	private OutputType outputType;
+	private String outputType;
 
 	/**
 	 * サービスカテゴリ
 	 */
 	@ApiModelProperty(value = "サービスカテゴリ", required = false, allowableValues = "見積(\"1\"), 契約(\"2\"), 手配(\3\")", example = "1", position = 2)
-	private ServiceCategory serviceCategory;
+	private String serviceCategory;
 
 	/**
 	 * 対象文書ID
@@ -42,7 +37,7 @@ public class ReportListParameter {
 	 * 対象種別
 	 */
 	@ApiModelProperty(value = "対象種別", required = false, allowableValues = "新規(\"1\"), 契約変更(\"2\"), 情報変更(\"3\"), 解約(\"4\")", example = "1", position = 4)
-	private TargetType targetType;
+	private String targetType;
 
 	/**
 	 * 商流区分
@@ -61,11 +56,11 @@ public class ReportListParameter {
 	 * ライフサイクル状態
 	 */
 	@ApiModelProperty(value = "ライフサイクル状態", required = false, allowableValues = "作成中(\"1\"), 作成完了(\"2\"), 受注(\"3\"), 失注(\"4\"), 破棄(\"5\"), キャンセル手続き中(\"6\"), 予定日待ち(\"7\"), 締結中(\"8\"), 解約手続き中(\"9\"), 解約予定日待ち(\"10\"), 解約(\"11\"), 旧契約(\"12\"), 締結待ち(\"13\")", example = "1", position = 8)
-	private LifecycleStatus lifecycleStatus;
+	private String lifecycleStatus;
 
 	/**
 	 * ワークフロー状態
 	 */
 	@ApiModelProperty(value = "ワークフロー状態", required = false, allowableValues = "作成中(\"1\"), 業務依頼中(\"2\"), 業務処理完了(\"3\"), 承認依頼中(\"4\"), 承認済(\"5\"), 顧客提示済(\"6\"), キャンセル申請中(\"7\"), 売上可能(\"8\"), 解約申請中(\"9\"), 手配中(\"10\"), 手配完了(\"11\"), 受付待ち(\"12\"), 作業中(\"13\"), 作業完了報告(\"14\"), 作業完了(\"15\"), エラー(\"16\")", example = "1", position = 9)
-	private WorkflowStatus workflowStatus;
+	private String workflowStatus;
 }
