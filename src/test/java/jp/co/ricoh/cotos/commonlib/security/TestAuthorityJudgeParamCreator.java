75af69aef87abb945acc08af288eb492198c91cb
chan-asa <Takahiro.Asakura@jrits.ricoh.co.jp>
2019-06-24 15:42:12 +0900

    権限判定用パラメーター作成処理に分岐処理を追加

--- src/test/java/jp/co/ricoh/cotos/commonlib/security/TestAuthorityJudgeParamCreator.java
+++ src/test/java/jp/co/ricoh/cotos/commonlib/security/TestAuthorityJudgeParamCreator.java
@@ -540,4 +540,38 @@ public class TestAuthorityJudgeParamCreator {
 		Assert.assertNotNull("正常に社員情報が作成されていること", authParam.getRequesterMvEmployeeMaster());
 		Assert.assertTrue("ユーザー直接指定であること", authParam.isManualApprover());
 	}
+	
+	@Test
+	public void 正常_権限判定用パラメーター取得_手配_手配無し() {
+
+		// ログインユーザー
+		MvEmployeeMaster actor = mvEmployeeMasterRepository.findByMomEmployeeId("00500784");
+
+		// 契約
+		Contract contract = new Contract();
+		contract.setLifecycleStatus(LifecycleStatus.作成中);
+
+		// 担当SA
+		contract.setContractPicSaEmp(new ContractPicSaEmp());
+		contract.getContractPicSaEmp().setMomEmployeeId("00500784");
+
+		// 追加編集者
+		contract.setContractAddedEditorEmpList(new ArrayList<>());
+		contract.getContractAddedEditorEmpList().add(new ContractAddedEditorEmp());
+		contract.getContractAddedEditorEmpList().get(0).setMomEmployeeId("00500784");
+
+		// 顧客
+		contract.setCustomerContract(new CustomerContract());
+		contract.getCustomerContract().setMomKjbSystemId("000000003985825");
+
+		AuthorityJudgeParameter authParam = authorityJudgeParamCreator.createFromArrangementWork(null, contract, actor, AccessType.参照);
+
+		Assert.assertNotNull("正常に社員情報が作成されていること", authParam.getMvEmployeeMasterList());
+		Assert.assertNotNull("正常に顧客情報が作成されていること", authParam.getVKjbMaster());
+		Assert.assertNotNull("正常にログインユーザー情報が作成されていること", authParam.getActorMvEmployeeMaster());
+		Assert.assertNull("正常に承認者の社員情報が作成されていないこと", authParam.getApproverMvEmployeeMasterList());
+		Assert.assertNull("正常に次回承認者の社員情報が作成されていないこと", authParam.getNextApproverMvEmployeeMaster());
+		Assert.assertNull("正常に承認依頼者の社員情報が作成されていないこと", authParam.getRequesterMvEmployeeMaster());
+		Assert.assertFalse("ユーザー直接指定でないこと", authParam.isManualApprover());
+	}
 }
