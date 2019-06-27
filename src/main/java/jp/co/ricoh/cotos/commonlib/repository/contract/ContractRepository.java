e5f031c26c698a623a769d7faa584fb3883a2221
akiko.kaga@jp.ricoh.com <akiko.kaga@jp.ricoh.com>
2019-06-27 18:24:49 +0900

    #3291 [UIバッチ新規] BTCOCR005_契約自動更新(SQL修正)

--- src/main/java/jp/co/ricoh/cotos/commonlib/repository/contract/ContractRepository.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/contract/ContractRepository.java
@@ -38,13 +38,13 @@ public interface ContractRepository extends CrudRepository<Contract, Long> {
 	@Query(value = "SELECT * FROM CONTRACT WHERE (CONTRACT_TYPE = '3' AND LIFECYCLE_STATUS = '5' AND CHANGE_PREFERRED_DATE <= :preferredDate) OR ((CONTRACT_TYPE = '1' OR CONTRACT_TYPE = '2') AND LIFECYCLE_STATUS = '11' AND CONCLUSION_PREFERRED_DATE <= :preferredDate)", nativeQuery = true)
 	public List<Contract> findByContractTypeAndPreferredDate(@Param("preferredDate") Date preferredDate);
 
-	@Query(value = "SELECT * FROM CONTRACT c, "
+	@Query(value = "SELECT c.* FROM CONTRACT c, "
 			+ "CONTRACT_DETAIL d, "
 			+ "ITEM_CONTRACT e, "
 			+ "CONTRACT_AUTO_UPDATE_MASTER a "
 			+ "WHERE (c.LIFECYCLE_STATUS = '6') AND "
 			+ "(c.id = d.contract_id AND d.id = e.contract_detail_id AND e.item_master_id = a.item_master_id AND a.contract_update_type = '1') AND "
-			+ "NOT EXISTS (SELECT * FROM CONTRACT WHERE (LIFECYCLE_STATUS = '5' OR LIFECYCLE_STATUS = '11')) AND "
+			+ "c.IMMUTABLE_CONT_IDENT_NUMBER NOT IN (SELECT ct.IMMUTABLE_CONT_IDENT_NUMBER FROM CONTRACT ct WHERE (ct.LIFECYCLE_STATUS = '5' OR ct.LIFECYCLE_STATUS = '11')) AND "
 			+ "c.SERVICE_TERM_END < :preferredDate", nativeQuery = true)
 	public List<Contract> findByAutoUpdaterecord(@Param("preferredDate") Date preferredDate);
 }
