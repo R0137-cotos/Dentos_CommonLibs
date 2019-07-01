de72a7a79440f2457a523ec484f3767a38aedca9
TomosadaKohhei <kohhei.tomosada@jp.ricoh.com>
2019-07-01 17:57:06 +0900

    #3290 MoM関連Repository・Entity追加

--- src/main/java/jp/co/ricoh/cotos/commonlib/repository/master/CommonMasterRepository.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/master/CommonMasterRepository.java
@@ -20,4 +20,6 @@ public interface CommonMasterRepository extends CrudRepository<CommonMaster, Lon
 
 	@Query(value = "SELECT CM.* FROM common_master CM INNER JOIN common_master_detail CMD ON CM.ID = CMD.COMMON_MASTER_ID WHERE CM.COLUMN_NAME = :COLUMN_NAME AND CM.SERVICE_CATEGORY = :SERVICE_CATEGORY AND CMD.CODE_VALUE IN (:CODE_VALUES)", nativeQuery = true)
 	public List<CommonMaster> findByColumnNameAndServiceCategoryAndDetailCodeValues(@Param("COLUMN_NAME") String columnName, @Param("SERVICE_CATEGORY") String serviceCategory, @Param("CODE_VALUES") List<String> codeValues);
+
+	public List<CommonMaster> findByColumnName(String columnName);
 }
