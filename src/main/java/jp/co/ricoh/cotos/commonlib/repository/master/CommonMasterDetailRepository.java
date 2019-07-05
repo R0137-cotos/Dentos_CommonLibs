dbd036c22c1926dc3874668f18b5df8b4f2f5764
akiko.kaga@jp.ricoh.com <akiko.kaga@jp.ricoh.com>
2019-07-05 16:16:45 +0900

    #3299 [UIバッチ改修] BTCOAC004_[CUBIC]仕訳データファイル作成(Add-On)

--- src/main/java/jp/co/ricoh/cotos/commonlib/repository/master/CommonMasterDetailRepository.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/master/CommonMasterDetailRepository.java
@@ -17,4 +17,7 @@ public interface CommonMasterDetailRepository extends CrudRepository<CommonMaste
 	// 受注完了時のメール送信でのみ使用可能
 	@Query(value = "FROM CommonMasterDetail WHERE COMMON_MASTER_ID = :COMMON_MASTER_ID AND TRUNC(CURRENT_DATE) BETWEEN TRUNC(AVAILABLE_PERIOD_FROM) AND TRUNC(AVAILABLE_PERIOD_TO) AND DATA_AREA_1 = :DATA_AREA_1 AND DELETE_FLG = 0")
 	public CommonMasterDetail findByCommonMasterIdAndDataArea1(@Param("COMMON_MASTER_ID") Long commonMasterId, @Param("DATA_AREA_1") String dataArea1);
+
+	@Query(value = "SELECT * FROM common_master_detail WHERE common_master_id = (select id from common_master where column_name = :COLUMN_NAME and rownum <= 1) AND to_date(:AVAILABLE_PERIOD, 'yyyymmdd') between trunc(available_period_from) and trunc(available_period_to) AND delete_flg = 0 and rownum <= 1", nativeQuery = true)
+	public CommonMasterDetail findByCommonMasterColumnNameAndAvailablePeriodBetween(@Param("COLUMN_NAME") String columnName, @Param("AVAILABLE_PERIOD") String availablePeriod);
 }
