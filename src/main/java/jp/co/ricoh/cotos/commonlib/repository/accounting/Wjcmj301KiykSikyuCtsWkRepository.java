473a3841e7647330436679e14e81359357c0096c
matsumura <yasuyuki.matsumura@jp.ricoh.com>
2019-07-05 14:34:40 +0900

    #3310 期間売内部振替情報のエンティティとリポジトリを作成

---
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/accounting/Wjcmj301KiykSikyuCtsWkRepository.java
@@ -0,0 +1,11 @@
+package jp.co.ricoh.cotos.commonlib.repository.accounting;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import jp.co.ricoh.cotos.commonlib.entity.accounting.Wjcmj301KiykSikyuCtsWk;
+
+@Repository
+public interface Wjcmj301KiykSikyuCtsWkRepository extends CrudRepository<Wjcmj301KiykSikyuCtsWk, String> {
+
+}
---
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/accounting/Wjcmj302SikyuMisiCtsWkRepository.java
@@ -0,0 +1,11 @@
+package jp.co.ricoh.cotos.commonlib.repository.accounting;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import jp.co.ricoh.cotos.commonlib.entity.accounting.Wjcmj302SikyuMisiCtsWk;
+
+@Repository
+public interface Wjcmj302SikyuMisiCtsWkRepository extends CrudRepository<Wjcmj302SikyuMisiCtsWk, String> {
+
+}
