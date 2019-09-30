3910cb045872fe197ca0e5f5f133f58033c5795f
Hiroaki Anada <hiroaki.anada@jp.ricoh.com>
2019-09-30 14:26:11 +0900

    月末最終営業日取得ロジックの追加

--- src/main/java/jp/co/ricoh/cotos/commonlib/logic/businessday/BusinessDayUtil.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/logic/businessday/BusinessDayUtil.java
@@ -2,11 +2,16 @@ package jp.co.ricoh.cotos.commonlib.logic.businessday;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 
+import jp.co.ricoh.cotos.commonlib.entity.master.BusinessCalendar;
 import jp.co.ricoh.cotos.commonlib.entity.master.NonBusinessDayCalendarMaster;
+import jp.co.ricoh.cotos.commonlib.repository.master.BusinessCalendarRepository;
 import jp.co.ricoh.cotos.commonlib.repository.master.NonBusinessDayCalendarMasterRepository;
 
 /**
@@ -17,6 +22,9 @@ public class BusinessDayUtil {
 
 	@Autowired
 	private NonBusinessDayCalendarMasterRepository nonBusinessDayCalendarMasterRepository;
+
+	@Autowired
+	private BusinessCalendarRepository BusinessCalendarRepository;
 
 	/**
 	 * 営業日かどうか
@@ -85,5 +93,83 @@ public class BusinessDayUtil {
 		}
 
 		return retDate;
+	}
+
+	/**
+	 * 引数月の業務カレンダーリスト取得
+	 * 
+	 * @param targetYm
+	 *            業務カレンダーを取得したい月
+	 * @return 業務カレンダーのリスト（1月分）
+	 */
+	public List<BusinessCalendar> findBusinessCalendarForSpecifiedMonth(String targetYm) {
+		int year = Integer.parseInt(StringUtils.substring(targetYm, 0, 4));
+		int month = StringUtils.length(targetYm) == 7 ? Integer.parseInt(StringUtils.substring(targetYm, 5, 7)) : Integer.parseInt(StringUtils.substring(targetYm, 4, 6));
+		return findBusinessCalendarForSpecifiedRange(getStartOfMonth(year, month), getEndOfMonth(year, month));
+	}
+
+	/**
+	 * 引数範囲の業務カレンダーリスト取得
+	 * 
+	 * @param targetPriodFrom
+	 *            業務カレンダー取得条件(From)
+	 * @param targetPriodTo
+	 *            業務カレンダー取得条件(To)
+	 * @return 業務カレンダーのリスト
+	 */
+	public List<BusinessCalendar> findBusinessCalendarForSpecifiedRange(Date targetPriodFrom, Date targetPriodTo) {
+		return BusinessCalendarRepository.findByBusinessDayBetweenOrderByBusinessDayDesc(targetPriodFrom, targetPriodTo);
+	}
+
+	/**
+	 * 引数月の月末最終営業日取得
+	 * 
+	 * @param targetYm
+	 *            月末最終営業日を取得したい月
+	 * @return 月末最終営業日
+	 */
+	public Date getLastBusinessDayOfTheMonth(String targetYm) {
+		BusinessCalendar result = findBusinessCalendarForSpecifiedMonth(targetYm).stream().findFirst().orElse(null);
+		return ObjectUtils.isEmpty(result) ? null : result.getBusinessDay();
+	}
+
+	/**
+	 * 引数年月の月初日取得
+	 * 
+	 * @param year
+	 *            年
+	 * @param month
+	 *            月
+	 * @return 月初日
+	 */
+	public Date getStartOfMonth(int year, int month) {
+		Calendar calendar = Calendar.getInstance();
+		// 指定年, 指定月, 1日, 0時0分0秒
+		calendar.set(year, month - 1, 1, 0, 0, 0);
+		// きっちりミリ秒も0にする
+		calendar.set(Calendar.MILLISECOND, 0);
+		return calendar.getTime();
+	}
+
+	/**
+	 * 引数年月の月末日取得
+	 * 
+	 * @param year
+	 *            年
+	 * @param month
+	 *            月
+	 * @return 月初日
+	 */
+	public Date getEndOfMonth(int year, int month) {
+		Calendar calendar = Calendar.getInstance();
+
+		// 指定年, 指定月(-1), 1日, 0時0分0秒
+		calendar.set(year, month - 1, 1, 0, 0, 0);
+		// 翌月にする (年またぎ対応)
+		calendar.add(Calendar.MONTH, 1);
+		// 0時0分0秒0ミリ秒 - 1ミリ秒で、月末23時59分59秒999ミリ秒にできる
+		calendar.set(Calendar.MILLISECOND, 0);
+		calendar.add(Calendar.MILLISECOND, -1);
+		return calendar.getTime();
 	}
 }
