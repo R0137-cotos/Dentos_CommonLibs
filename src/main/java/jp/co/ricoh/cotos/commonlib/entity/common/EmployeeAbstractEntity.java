accc048cc0fec6b1b88ab4a6b68894263c65c6bc
Kanda Masashi <mkanda000700@rfg.rfgad.ricoh.com>
2019-10-03 15:38:53 +0900

    Swagger修正

--- src/main/java/jp/co/ricoh/cotos/commonlib/entity/common/EmployeeAbstractEntity.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/entity/common/EmployeeAbstractEntity.java
@@ -33,84 +33,83 @@ public class EmployeeAbstractEntity extends EntityBase {
 	 * 所属組織MoM組織ID
 	 */
 	@Size(max = 255)
-	@ApiModelProperty(value = "所属組織MoM組織ID(作成時不要)", required = false, position = 52, allowableValues = "range[0,255]", readOnly = true)
+	@ApiModelProperty(value = "所属組織MoM組織ID", required = false, position = 52, allowableValues = "range[0,255]", readOnly = false)
 	private String momOrgId;
 
 	/**
 	 * 所属組織階層レベル
 	 */
-	@ApiModelProperty(value = "所属組織階層レベル(作成時不要)", required = false, position = 53, allowableValues = "range[0,9]", readOnly = true)
+	@ApiModelProperty(value = "所属組織階層レベル", required = false, position = 53, allowableValues = "range[0,9]", readOnly = false)
 	private Integer orgHierarchyLevel;
 
 	/**
 	 * 所属組織名
 	 */
 	@Size(max = 255)
-	@ApiModelProperty(value = "所属組織名(作成時不要)", required = false, position = 54, allowableValues = "range[0,255]", readOnly = true)
+	@ApiModelProperty(value = "所属組織名", required = false, position = 54, allowableValues = "range[0,255]", readOnly = false)
 	private String orgName;
 
 	/**
 	 * 販売会社名
 	 */
 	@Size(max = 255)
-	@ApiModelProperty(value = "販売会社名(作成時不要)", required = false, position = 55, allowableValues = "range[0,255]", readOnly = true)
+	@ApiModelProperty(value = "販売会社名", required = false, position = 55, allowableValues = "range[0,255]", readOnly = false)
 	private String salesCompanyName;
 
 	/**
 	 * 会社代表電話番号
 	 */
 	@Size(max = 255)
-	@ApiModelProperty(value = "会社代表電話番号(作成時不要)", required = false, position = 56, allowableValues = "range[0,255]", readOnly = true)
+	@ApiModelProperty(value = "会社代表電話番号", required = false, position = 56, allowableValues = "range[0,255]", readOnly = false)
 	private String orgPhoneNumber;
 
 	/**
 	 * 社員名
 	 */
-	@Column(nullable = false)
 	@Size(max = 255)
-	@ApiModelProperty(value = "社員名(作成時不要)", required = true, position = 57, allowableValues = "range[0,255]", readOnly = true)
+	@ApiModelProperty(value = "社員名", required = false, position = 57, allowableValues = "range[0,255]", readOnly = false)
 	private String employeeName;
 
 	/**
 	 * 部署名
 	 */
 	@Size(max = 255)
-	@ApiModelProperty(value = "部署名(作成時不要)", required = false, position = 58, allowableValues = "range[0,255]", readOnly = true)
+	@ApiModelProperty(value = "部署名", required = false, position = 58, allowableValues = "range[0,255]", readOnly = false)
 	private String salesDepartmentName;
 
 	/**
 	 * 郵便番号
 	 */
 	@Size(max = 255)
-	@ApiModelProperty(value = "郵便番号(作成時不要)", required = false, position = 59, allowableValues = "range[0,255]", readOnly = true)
+	@ApiModelProperty(value = "郵便番号", required = false, position = 59, allowableValues = "range[0,255]", readOnly = false)
 	private String postNumber;
 
 	/**
 	 * 住所
 	 */
 	@Size(max = 1000)
-	@ApiModelProperty(value = "住所(作成時不要)", required = false, position = 60, allowableValues = "range[0,1000]", readOnly = true)
+	@ApiModelProperty(value = "住所", required = false, position = 60, allowableValues = "range[0,1000]", readOnly = false)
 	private String address;
 
 	/**
 	 * 電話番号
 	 */
 	@Size(max = 255)
-	@ApiModelProperty(value = "電話番号(作成時不要)", required = false, position = 61, allowableValues = "range[0,255]", readOnly = true)
+	@ApiModelProperty(value = "電話番号", required = false, position = 61, allowableValues = "range[0,255]", readOnly = false)
 	private String phoneNumber;
 
 	/**
 	 * FAX番号
 	 */
 	@Size(max = 255)
-	@ApiModelProperty(value = "FAX番号(作成時不要)", required = false, position = 62, allowableValues = "range[0,255]", readOnly = true)
+	@ApiModelProperty(value = "FAX番号", required = false, position = 62, allowableValues = "range[0,255]", readOnly = false)
 	private String faxNumber;
 
 	/**
 	 * メールアドレス
 	 */
 	@Size(max = 255)
-	@ApiModelProperty(value = "メールアドレス(作成時不要)", required = false, position = 63, allowableValues = "range[0,255]", readOnly = true)
+	@ApiModelProperty(value = "メールアドレス", required = false, position = 63, allowableValues = "range[0,255]", readOnly = false)
 	private String mailAddress;
 
 }
