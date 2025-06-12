146d5e7b752b9f403ca26927c0f17e2f4a05d771
Kazuki Itagaki <kazuki.itagaki@jp.ricoh.com>
2025-06-12 12:46:26 +0900

    30092_修正漏れ対応

--- src/main/java/jp/co/ricoh/cotos/commonlib/repository/master/MailTemplateMasterRepository.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/master/MailTemplateMasterRepository.java
@@ -9,7 +9,7 @@ import jp.co.ricoh.cotos.commonlib.entity.master.MailTemplateMaster;
 
 @Repository
 public interface MailTemplateMasterRepository extends CrudRepository<MailTemplateMaster, Long> {
-	@Query(value = "FROM MailTemplateMaster WHERE service_category = :SERVICE_CATEGORY AND process_category = :PROCESS_CATEGORY", nativeQuery = true)
+	@Query(value = "SELECT * FROM mail_template_master WHERE service_category = :SERVICE_CATEGORY AND process_category = :PROCESS_CATEGORY", nativeQuery = true)
 	public MailTemplateMaster findByServiceCategoryAndProcessCategory(@Param("SERVICE_CATEGORY") String serviceCategory, @Param("PROCESS_CATEGORY") String processCategory);
 
 	@Query(value = "SELECT * FROM (SELECT * FROM mail_template_master WHERE service_category = :SERVICE_CATEGORY AND process_category = :PROCESS_CATEGORY AND (product_grp_master_id = :PRODUCT_GRP_MASTER_ID OR product_grp_master_id = 0) order by product_grp_master_id desc) WHERE rownum = 1 ", nativeQuery = true)
