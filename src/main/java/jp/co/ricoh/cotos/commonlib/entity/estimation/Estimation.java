5d0c22608f4dda7b02665c386f258092393b71aa
Kazuki Itagaki <kazuki.itagaki@jp.ricoh.com>
2025-07-09 08:52:14 +0900

    30826_ToStringアノテーション修正

--- src/main/java/jp/co/ricoh/cotos/commonlib/entity/estimation/Estimation.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/entity/estimation/Estimation.java
@@ -37,12 +37,14 @@ import jp.co.ricoh.cotos.commonlib.entity.EntityBase;
 import jp.co.ricoh.cotos.commonlib.security.CotosAuthenticationDetails;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 /**
  * 見積を表すEntityです。
  */
 @Entity
 @EqualsAndHashCode(callSuper = true, exclude={"estimationApprovalRoute","estimationPicSaEmp","customerEstimation", "estimationCheckResultList", "operationLogList", "estimationAttachedFileList", "estimationAddedEditorEmpList", "dealerEstimationList", "estimationDetailList", "productEstimationList"})
+@ToString(callSuper = true, exclude = {"estimationApprovalRoute","estimationPicSaEmp","customerEstimation", "estimationCheckResultList", "operationLogList", "estimationAttachedFileList", "estimationAddedEditorEmpList", "dealerEstimationList", "estimationDetailList", "productEstimationList"})
 @EntityListeners(EstimationListener.class)
 @Data
 @Table(name = "estimation")
